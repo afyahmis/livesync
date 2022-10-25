2cb1652338ef4551e4d204abd36295ad665603b4
koskedk <koskedk@live.com>
2022-10-25 14:02:49 +0300

    initial commit

---
+++ src/client/LiveSync.Client/Controllers/GreeterController.cs
@@ -0,0 +1,29 @@
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using GrpcLiveSyncClient;
+
+namespace LiveSync.Client.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GreeterController : ControllerBase
+{
+    private readonly ILogger<GreeterController> _logger;
+    private readonly GrpcChannel channel;
+
+    public GreeterController(ILogger<GreeterController> logger)
+    {
+        _logger = logger;
+        channel=GrpcChannel.ForAddress("http://localhost:5193");
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Greet(string name)
+    {
+        var client = new Greeter.GreeterClient(channel);
+        
+        var reply = client.SayHello(
+            new HelloRequest { Name = name });
+        return Ok($"{reply}");
+    }
+}
